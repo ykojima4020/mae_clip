6968b64bcf7d28753c31b49b7189778ad1dfb191
Yuto Kojima <yuto.kojima@sony.com>
2024-02-15 15:27:07 -0800

    adopt GroupViT's loss calculation method

--- model/clip.py
+++ model/clip.py
@@ -3,6 +3,8 @@ import torch.distributed as dist
 from torch import nn
 import torch.nn.functional as F
 
+import diffdist.functional as diff_dist
+
 class CLIP(nn.Module):
     def __init__(self, image_encoder, text_encoder, image_projection, text_projection, temperature=0.07):
         super().__init__()
@@ -12,6 +14,7 @@ class CLIP(nn.Module):
         self._text_projection = text_projection
         self._temperature = 0.07
         self.logit_scale = nn.Parameter(torch.ones([]) *  self._temperature)
+        self.cross_entropy = nn.CrossEntropyLoss()
 
     def image_encode(self, image):
         # Getting Image and Text Features
@@ -26,29 +29,23 @@ class CLIP(nn.Module):
 
     def loss(self, image_x, text_x):
         batch_size = image_x.shape[0]
+        # get label globally
+        labels = torch.arange(batch_size, dtype=torch.long, device=image_x.device) + batch_size * dist.get_rank()
 
-        logit_scale = torch.clamp(self.logit_scale.exp(), max=100)
+        # [B, C]
         image_x = F.normalize(image_x, dim=-1)
         text_x = F.normalize(text_x, dim=-1)
 
-        image_x_all = concat_all_gather(image_x)
-        text_x_all = concat_all_gather(text_x)
- 
-        images_similarity = image_x_all @ image_x_all.T
-        texts_similarity = text_x_all @ text_x_all.T
-        targets = F.softmax(((images_similarity + texts_similarity) / 2) * logit_scale, dim=-1) # [B, B]
+        logits_per_img = image_x @ dist_collect(text_x).t()
+        logits_per_text = text_x @ dist_collect(image_x).t()
 
-        start = dist.get_rank() * batch_size
-        end = start + batch_size
-        targets = targets[start:end]             # [B/gpus, B]
+        logit_scale = torch.clamp(self.logit_scale.exp(), max=100)
+        loss_img = self.cross_entropy(logits_per_img * logit_scale, labels)
+        loss_text = self.cross_entropy(logits_per_text * logit_scale, labels)
 
-        logits_per_img = image_x @ text_x_all.T  # [B/gpus, B]
-        logits_per_text = text_x @ image_x_all.T # [B/gpus, B]
-
-        texts_loss = cross_entropy(logits_per_text, targets, reduction='mean')
-        images_loss = cross_entropy(logits_per_img, targets, reduction='mean')
-        loss =  (images_loss + texts_loss) / 2.0
+        loss = 0.5 * (loss_img + loss_text)
         return loss, logit_scale
+
 
     def forward(self, batch):
         image_x = self.image_encode(batch['image'])
@@ -60,29 +57,19 @@ class CLIP(nn.Module):
     def get_transforms(self, mode):
         return self._image_encoder.get_transforms(mode)
 
-def cross_entropy(preds, targets, reduction='none'):
-    log_softmax = nn.LogSoftmax(dim=-1)
-    loss = (-targets * log_softmax(preds)).sum(1)
-    if reduction == "none":
-        return loss
-    elif reduction == "mean":
-        return loss.mean()
-
-# utils
-# this is a function from https://github.com/facebookresearch/moco/blob/main/moco/builder.py#L178
-@torch.no_grad()
-def concat_all_gather(tensor):
+#[NOTE]: https://github.com/NVlabs/GroupViT/blob/main/models/multi_label_contrastive.py#L24C1-L34C51
+def dist_collect(x):
+    """ collect all tensor from all GPUs
+    args:
+        x: shape (mini_batch, ...)
+    returns:
+        shape (mini_batch * num_gpu, ...)
     """
-    Performs all_gather operation on the provided tensors.
-    *** Warning ***: torch.distributed.all_gather has no gradient.
-    """
-    tensors_gather = [
-        torch.ones_like(tensor) for _ in range(torch.distributed.get_world_size())
-    ]
-    torch.distributed.all_gather(tensors_gather, tensor, async_op=False)
+    x = x.contiguous()
+    out_list = [torch.zeros_like(x, device=x.device, dtype=x.dtype).contiguous() for _ in range(dist.get_world_size())]
+    out_list = diff_dist.all_gather(out_list, x)
+    return torch.cat(out_list, dim=0).contiguous()
 
-    output = torch.cat(tensors_gather, dim=0)
-    return output
 
 if __name__ == '__main__':
     images = torch.randn(8, 3, 224, 224)
