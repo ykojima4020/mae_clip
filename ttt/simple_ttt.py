c4c79ab41401a42e7a4d671f797bc5398377f2ab
Yuto Kojima <yuto.kojima@sony.com>
2024-03-09 15:57:37 -0800

    wandb sweep

--- ttt/simple_ttt.py
+++ ttt/simple_ttt.py
@@ -1,29 +1,29 @@
 import argparse
-
 import torch
 import sys
 from tqdm import tqdm
 import pandas as pd
+import wandb
 
 sys.path.append('../')
 from evaluator.evaluator import ZeroShotImageNetEvaluator
-from transformers import DistilBertTokenizer
 from factory import RILSMAECLIPFactory, PretrainedOpenCLIPFactory
 from misc.config import load_config
 from omegaconf import OmegaConf
 
 from imagenetv2_pytorch import ImageNetV2Dataset
-from misc.transforms import Corruption, get_original_vit_image_encoder_transforms, get_corruption_transform
+from misc.transforms import Corruption, get_corruption_transform
 
 
 corruptions_name = ['brightness', 'contrast', 'defocus_blur', 'elastic_transform', 'fog',
-               'frost', 'gaussian_noise', 'glass_blur', 'impulse_noise', 'jpeg_compression',
-               'motion_blur', 'pixelate', 'shot_noise', 'snow', 'zoom_blur']
+                    'frost', 'gaussian_noise', 'glass_blur', 'impulse_noise', 'jpeg_compression',
+                    'motion_blur', 'pixelate', 'shot_noise', 'snow', 'zoom_blur']
 
 def get_args_parser():
     parser = argparse.ArgumentParser('Evaluation on ImageNet-C', add_help=False)
     parser.add_argument('--cfg', type=str, required=True, help='path to a config file')
     parser.add_argument('--type', default='normal', choices=['normal', 'open'], help='a kind of archtectures')
+    parser.add_argument('--wandb', action='store_true')
     parser.add_argument('--checkpoint', type=str, required=True, help='path to a pth file')
     parser.add_argument('--output', type=str, default='./output.csv', help='path to a output csv file')
     return parser
@@ -31,12 +31,12 @@ def get_args_parser():
 
 class TestTimeTrainer():
 
-    def __init__(self, data_loader, optimizer, device):
+    def __init__(self, data_loader, optimizer, mask_ratio, device):
         self._data_loader = data_loader
         self._optimizer = optimizer
         self._device = device
 
-        self._mask_ratio = 0.75
+        self._mask_ratio = mask_ratio
 
     def __call__(self, model):
         tqdm_object = tqdm(self._data_loader, total=len(self._data_loader))
@@ -50,22 +50,29 @@ class TestTimeTrainer():
             self._optimizer.step()
             self._optimizer.zero_grad()
 
+def sweep():
+    sweep_config = load_config('ttt_sweep.yaml')
+    sweep_id = wandb.sweep(OmegaConf.to_container(sweep_config, resolve=True),
+                           project="mae_clip_ttt")
+    wandb.agent(sweep_id, main)
 
 def main():
     args = get_args_parser()
     args = args.parse_args()
 
-    batch_size = 64
-    num_workers = 4
-    pin_memory = True
-    lr = 1e-3 * batch_size / 256
+    # fixed parameters
     eps = 1e-8 
-    weight_decay = 0.05 
     device = 'cuda'
-    total_epoch = 1
 
     cfg = load_config(args.cfg)
     OmegaConf.set_struct(cfg, True)    
+
+    if args.wandb:
+        run = wandb.init(project='mae_clip_ttt',
+                         entity="ykojima",
+                         config=OmegaConf.to_container(cfg, resolve=True))
+        config = wandb.config
+        print(config)
 
     if args.type == 'normal':
         factory = RILSMAECLIPFactory(cfg.model)
@@ -74,69 +81,62 @@ def main():
     else:
         raise TypeError
 
-    model, tokenizer, transform = factory.create()
+    model, tokenizer, _ = factory.create()
     model = model.to(device)
+    # [NOTE]: freze CLIP parameters
+    for name, param in model.named_parameters():
+        if ('image_encoder' in name):
+            param.requires_grad = True
+        else:
+            param.requires_grad = False
+        # print(name, param.requires_grad)
  
     status = torch.load(args.checkpoint, map_location="cuda")
 
-    severities = []
-    corruptions = []
-    acc_1 = []
-    acc_5 = []
-    ttt_acc_1 = []
-    ttt_acc_5 = []
+    severity = 5
+    corruption = "gaussian_noise"
 
-    for severity in range(1,6):
-        for corruption in corruptions_name:
- 
-            severities.append(severity)
-            corruptions.append(corruption)
+    lr = config.lr_scale * 1e-3 * config.batch_size / 256
+    optimizer = torch.optim.AdamW(model.mae.parameters(),
+                eps=eps, lr=lr, betas=(0.9, 0.95), weight_decay=config.weight_decay)
+    top1, top5 = run_ttt(model, tokenizer, status, severity, corruption, optimizer, epochs=config.epochs, batch_size=config.batch_size)
 
-            model.load_state_dict(status['model'])
-        
-            # [NOTE]: freze CLIP parameters
-            for name, param in model.named_parameters():
-                if ('mae_decoder' in name) or ('image_encoder' in name):
-                    param.requires_grad = True
-                else:
-                    param.requires_grad = False
-        
-            transform = get_corruption_transform(Corruption(severity=severity, corruption_name=corruption))
-            evaluator = ZeroShotImageNetEvaluator(tokenizer, device, transform)
-            eval_res = evaluator(model.clip)
-            print('before test time training')
-            print(eval_res)
-            acc_1.append(eval_res['eval']['imagenet']['top1'])
-            acc_5.append(eval_res['eval']['imagenet']['top5'])
-        
-            train_dataset = ImageNetV2Dataset(transform=transform)
-            train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, num_workers=num_workers, pin_memory=pin_memory)
-        
-            # optimizer, but no scheduler 
-            optimizer = torch.optim.AdamW(model.mae.parameters(),
-                        eps=eps, lr=lr, betas=(0.9, 0.95), weight_decay=weight_decay)
-        
-            tttrainer = TestTimeTrainer(train_loader, optimizer, device)
-        
-            for epoch in range(0, total_epoch):
-                tttrainer(model.mae)
-        
-            eval_res = evaluator(model.clip)
-            print('after test time training')
-            print(eval_res)
-            ttt_acc_1.append(eval_res['eval']['imagenet']['top1'])
-            ttt_acc_5.append(eval_res['eval']['imagenet']['top5'])
+    stats = {'top1': top1, 'top5': top5}
+    if args.wandb:
+        wandb.log(stats)
+        # table.add_data(lr, weight_decay, total_epoch, batch_size, severity, corruption, top1, top5)
 
-    df = pd.DataFrame({'severity': severities,
-                       'corruption': corruptions,
-                       'top1': acc_1,
-                       'top5': acc_5,
-                       'ttt_top1': ttt_acc_1,
-                       'ttt_top5': ttt_acc_5, })
-    print(df) 
-    df.to_csv(args.output, index=False)   
+    del model
+    torch.cuda.empty_cache()
 
+    # table.get_dataframe().to_csv(args.output, index=False)
+    # wandb.log({'result': table})
+
+    if args.wandb:
+        run.finish()
+
+def run_ttt(model, tokenizer, status, severity, corruption, optimizer,
+            epochs=1, batch_size=64, num_workers=4, pin_memory=True,
+            mask_ratio=0.75, device='cuda', ttt=True):
+    model.load_state_dict(status['model'])
+
+    transform = get_corruption_transform(Corruption(severity=severity, corruption_name=corruption))
+    evaluator = ZeroShotImageNetEvaluator(tokenizer, device, transform)
+
+    train_dataset = ImageNetV2Dataset(transform=transform)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, num_workers=num_workers, pin_memory=pin_memory)
+
+    if ttt:
+        tttrainer = TestTimeTrainer(train_loader, optimizer, mask_ratio, device)
+
+        for epoch in range(0, epochs):
+            tttrainer(model.mae)
+
+    eval_res = evaluator(model.clip)
+    top1 = eval_res['eval']['imagenet']['top1']
+    top5 = eval_res['eval']['imagenet']['top5']
+    return top1, top5 
 
 if __name__ == "__main__":
     main()
-
+    # sweep()
